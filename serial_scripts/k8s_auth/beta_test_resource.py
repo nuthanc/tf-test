8915b984bb0f96f002222bd470487abda7d553ec
nuthanc <nuthanc@juniper.net>
2020-11-26 01:58:42 -0800

    Working manual_test and all_in_one_policy

--- serial_scripts/k8s_auth/beta_test_resource.py
+++ serial_scripts/k8s_auth/beta_test_resource.py
@@ -183,13 +183,14 @@ def manual_test2():
     match, stackrc_dict = ResourceUtil.create_test_user_openstack_objects_and_return_match_list_and_stackrc_dict()
     create_policy.create_and_apply_policies(resource=resource, match=match)
 
-manual_test2()
+# manual_test2()
 
 # MSG Check the comments in all_in_one
 def manual_test():
     # Create the required users, projects and domains
     admin = ExampleUser.admin()
     
+
     admin.create_all(user_name='zoro', password='c0ntrail123', role='Member',
                      project_name='zoro_project', domain_name='zoro_domain')
     admin.create_all(user_name='ola', password='c0ntrail123', role='Member',
@@ -198,8 +199,11 @@ def manual_test():
                      project_name='uber_project', domain_name='uber_domain')
     admin.create_all(user_name='zomato', password='c0ntrail123', role='Member',
                      project_name='zomato_project', domain_name='zomato_domain')
+
+    ResourceUtil.source_stackrc(**ResourceUtil.admin_stackrc())
     os.system('kubectl create ns zomsrc')
-    os.system('juju config kubernetes-master keystone-policy="$(cat /root/nuthanc-tf-test/tcutils/kubernetes/auth/templates/all_in_one_policy.yaml)')
+    os.system('kubectl create ns easy')
+    os.system('juju config kubernetes-master keystone-policy="$(cat /root/nuthanc-tf-test/tcutils/kubernetes/auth/templates/all_in_one_policy.yaml)"')
     import pdb;pdb.set_trace()
     # kubectl describe configmap -n kube-system k8s-auth-policy
     # Stackrcs in o7k
@@ -209,10 +213,10 @@ def manual_test():
         'user_name': 'ola',
         'password': 'c0ntrail123',
         'project_name': 'ola_project',
-        'domain_name': 'uber_domain',
+        'domain_name': 'ola_domain',
         'auth_url': admin.auth_url
     }
-    resource_expectation_list = ['pod-expected', 'deployment', 'service', 'namespace',
+    resource_expectation_list = ['pod-expected', 'deployment-expected', 'service', 'namespace',
                                  'network_attachment_definition', 'network_policy', 'ingress', 'daemonset']
     ResourceUtil.perform_operations(
         stackrc_dict=stackrc_dict, resource_expectation_list=resource_expectation_list)
@@ -226,7 +230,7 @@ def manual_test():
         'domain_name': 'uber_domain',
         'auth_url': admin.auth_url
     }
-    resource_expectation_list = ['pod', 'deployment-expected', 'service', 'namespace',
+    resource_expectation_list = ['pod-expected', 'deployment-expected', 'service', 'namespace',
                                  'network_attachment_definition', 'network_policy', 'ingress', 'daemonset']
     ResourceUtil.perform_operations(
         stackrc_dict=stackrc_dict, resource_expectation_list=resource_expectation_list)
@@ -244,8 +248,7 @@ def manual_test():
     ResourceUtil.perform_operations(
         stackrc_dict=stackrc_dict, resource_expectation_list=resource_expectation_list)
 
-    # For zoro user
-    os.system('kubectl create ns easy')
+    # For zoro user, any operation on pods, deployments and services but only in easy namespace
     stackrc_dict = {
         'user_name': 'zoro',
         'password': 'c0ntrail123',
@@ -254,10 +257,11 @@ def manual_test():
         'auth_url': admin.auth_url
     }
     resource_expectation_list = ['pod-expected', 'deployment-expected', 'service-expected', 'namespace-expected',
-                                 'network_attachment_definition-expected', 'network_policy-expected', 'ingress-expected', 'daemonset-expected']
+                                 'network_attachment_definition', 'network_policy', 'ingress', 'daemonset']
     ResourceUtil.perform_operations(
         resource_expectation_list=resource_expectation_list, stackrc_dict=stackrc_dict)
     
+manual_test()
 
 
 
