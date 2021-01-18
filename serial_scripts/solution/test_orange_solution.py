0e4d215ed294edcb400ebf5815b782f3b93a0776
root <root@nodem14.englab.juniper.net>
2021-01-18 07:09:41 +0000

    Add vsrx.yaml and add setup_vsrx method

--- serial_scripts/solution/test_orange_solution.py
+++ serial_scripts/solution/test_orange_solution.py
@@ -38,6 +38,33 @@ class OrangeSolutionTest(BaseSolutionsTest):
         super(OrangeSolutionTest, cls).tearDownClass()
 
     @classmethod
+    def setup_vsrx(cls):
+        #Create vSRX with sub-interfaces
+        cls.vsrx_template_file=cls.deploy_path+"template/vsrx.yaml"
+        with open(cls.vsrx_template_file, 'r') as fd:
+            cls.vsrx_template = yaml.load(fd, Loader=yaml.FullLoader)
+
+        for each_resource in cls.vsrx_template['resources']:
+            if 'personality' in cls.vsrx_template['resources']\
+                                    [each_resource]['properties']:
+                inject_file='/config/junos-config/configuration.txt'
+                fp1=open(cls.vsrx_template['resources'][each_resource]\
+                                ['properties']['personality'][inject_file]\
+                                ['get_file'], 'r')
+                data=fp1.read()
+                cls.vsrx_template['resources'][each_resource]['properties']\
+                    ['personality'][inject_file]=data
+                fp1.close()
+
+        cls.vsrx_stack = HeatStackFixture(
+                                connections=cls.connections,
+                                stack_name=cls.connections.project_name+'_vsrx_scale',
+                                template=cls.vsrx_template,
+                                timeout_mins=15)
+        # cls.vsrx_stack.setUp()
+
+
+    @classmethod
     def setup_vepg(cls):
 
     #Quota Create.
