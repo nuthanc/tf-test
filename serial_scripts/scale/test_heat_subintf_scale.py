4d433a8881be1af891db0658a3885737501f7d2c
Nuthan Chandra <nuthanc@juniper.net>
2021-02-09 11:49:22 +0000

    Initial mods for having multiple stacks

--- serial_scripts/scale/test_heat_subintf_scale.py
+++ serial_scripts/scale/test_heat_subintf_scale.py
@@ -10,6 +10,7 @@ from nova_test import *
 from vm_test import *
 from jinja2 import Environment, FileSystemLoader
 import yaml
+from ipaddress import IPv4Network
 
 
 class SubIntfScaleTest(BaseScaleTest):
@@ -19,8 +20,8 @@ class SubIntfScaleTest(BaseScaleTest):
     def setUpClass(cls):
         super(SubIntfScaleTest, cls).setUpClass()
         #Can update deployment path based on variable.
-        cls.deploy_path = os.getenv('DEPLOYMENT_PATH',
-                                    'serial_scripts/scale/')
+        cls.template_path = os.getenv('DEPLOYMENT_PATH',
+                                    'serial_scripts/scale/template')
         cls.setup_vsrx()
 
     @classmethod
@@ -31,21 +32,14 @@ class SubIntfScaleTest(BaseScaleTest):
 
     @classmethod
     def setup_vsrx(cls):
-        #Create vSRX with sub-interfaces
-        cls.vsrx_image = cls.nova_h.get_image('vsrx')
-        cls.flavor = cls.nova_h.get_flavor('contrail_flavor_2cpu')
-        cls.vsrx_template_file = cls.deploy_path+"template/vsrx.yaml"
-        with open(cls.vsrx_template_file, 'r') as fd:
-            cls.vsrx_template = yaml.load(fd, Loader=yaml.FullLoader)
-
+        cls.nova_h.get_image('vsrx')
+        cls.nova_h.get_flavor('contrail_flavor_2cpu')
+        cls.vsrx_template = f'{cls.template_path}/vsrx.yaml'
         cls.vsrx_stack = HeatStackFixture(
             connections=cls.connections,
             stack_name=cls.connections.project_name+'_vsrx_scale',
             template=cls.vsrx_template,
             timeout_mins=15)
-
-        
-
 
         cls.vsrx_stack.setUp()
         op = cls.vsrx_stack.heat_client_obj.stacks.get(
@@ -55,8 +49,8 @@ class SubIntfScaleTest(BaseScaleTest):
         vsrx.read()
         vsrx.verify_on_setup()
         vsrx.vm_password = 'c0ntrail123'
-        file1 = f'/contrail-test/{cls.deploy_path}template/junos_config.txt'
-        file2 = f'/contrail-test/{cls.deploy_path}template/config.sh'
+        file1 = f'/contrail-test/{cls.template_path}/junos_config.txt'
+        file2 = f'/contrail-test/{cls.template_path}/config.sh'
         vsrx.copy_file_to_vm(localfile=file1, dstdir='/root')
         vsrx.copy_file_to_vm(localfile=file2, dstdir='/root')
         cls.inputs.run_cmd_on_server(vsrx.vm_node_ip, 'yum install -y sshpass')
@@ -69,53 +63,55 @@ class SubIntfScaleTest(BaseScaleTest):
         op = os.popen(cmd).read()
         import pdb;pdb.set_trace()
         
-
-    @staticmethod
-    def load_template():
-        from ipaddress import IPv4Network
-        network = '27.7.57.0'
-        mask = '29'
-        parent_ip = '27.7.57.3'
+    @classmethod
+    def generate_network_objects(cls, num):
+        cls.parent_ip = '27.7.57.3'
         cidr = IPv4Network("17.27.0.0/16")
         # for sn in cidr.subnets(new_prefix=28):
-        ips = []
-        neighbor1_list = []
-        neighbor2_list = []
-        sub_intf_nets = []
-        sub_intf_masks = []
-        sub_mask = 22
-        for sn in cidr.subnets(new_prefix=sub_mask):
+        cls.ips = []
+        cls.neighbor1_list = []
+        cls.neighbor2_list = []
+        cls.sub_intf_nets = []
+        cls.sub_intf_masks = []
+        cls.sub_mask = 28
+        cls.local_as = 64500
+        for n, sn in enumerate(cidr.subnets(new_prefix=sub_mask)):
+            if n == num:
+                break
             sub_intf_cidr = IPv4Network(sn)
             sub_intf_net = str(sub_intf_cidr.network_address)
             sub_intf_mask = sub_intf_cidr.prefixlen
-            sub_intf_nets.append(sub_intf_net)
-            sub_intf_masks.append(sub_intf_mask)
+            cls.sub_intf_nets.append(sub_intf_net)
+            cls.sub_intf_masks.append(sub_intf_mask)
             for i,ip in enumerate(sub_intf_cidr):
                 if i == 0:
                     continue
                 elif i == 1:
-                    neighbor1_list.append(ip)
+                    cls.neighbor1_list.append(ip)
                 elif i == 2:
-                    neighbor2_list.append(ip)
+                    cls.neighbor2_list.append(ip)
                 elif i == 3:
-                    ips.append(ip)
+                    cls.ips.append(ip)
                 else:
                     break
-        local_as = 64500
-        deploy_path = os.getenv('DEPLOYMENT_PATH',
-                                '/contrail-test/serial_scripts/scale/')
-        template_dir = deploy_path+"template/"
-        env = Environment(loader=FileSystemLoader(template_dir))
+        
+    @classmethod
+    def load_template(cls):
+        num = 10
+        cls.generate_network_objects(num)
+        env = Environment(loader=FileSystemLoader(cls.template_path))
         vsrx_temp = env.get_template("vsrx.yaml.j2")
         junos_temp = env.get_template("junos_config.txt.j2")
-        filename1 = template_dir + "vsrx.yaml"
-        filename2 = template_dir + "junos_config.txt"
-        with open(filename1, 'w') as f:
-            f.write(vsrx_temp.render(ips=ips, network=network,
-                                     mask=mask, sub_intf_nets=sub_intf_nets, sub_intf_masks=sub_intf_masks))
-        with open(filename2, 'w') as f:
-            f.write(junos_temp.render(ips=ips, local_as=local_as,
-                                      parent_ip=parent_ip, neighbor1_list=neighbor1_list, neighbor2_list=neighbor2_list, sub_mask=sub_mask))
+
+        # Need to have multiple sub files
+        sub_intf_file = f'{cls.template_path}/sub_intf.yaml'
+        junos_file = f'{cls.template_path}/junos_config.txt'
+        with open(sub_intf_file, 'w') as f:
+            f.write(vsrx_temp.render(ips=cls.ips, network=cls.port_network,
+                                     mask=cls.port_net_mask, sub_intf_nets=cls.sub_intf_nets, sub_intf_masks=cls.sub_intf_masks))
+        with open(junos_file, 'w') as f:
+            f.write(junos_temp.render(ips=cls.ips, local_as=cls.local_as,
+                                      parent_ip=cls.parent_ip, neighbor1_list=cls.neighbor1_list, neighbor2_list=cls.neighbor2_list, sub_mask=cls.sub_mask))
 
 
 
